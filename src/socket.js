6ee963629879b7d660a92f28fd8ea1dc8f4b6826
Srinivasalu03 <iamseenu2003@gmail.com>
2025-10-31 00:17:36 +0530

    Corrected server URL

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // URL of our backend server
-const URL = 'https://whiteboard-server-lfuq.onrender.com';
+const URL = 'https://whiteboard-server-0tkj.onrender.com';
 
 // Create the socket instance
 // 'autoConnect: false' means it won't connect until we explicitly call socket.connect()
